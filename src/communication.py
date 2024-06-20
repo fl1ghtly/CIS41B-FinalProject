6389af59b20fdd641de9103e056a398591a1f199
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2024-06-19 21:48:06 -0700

    more comments

--- src/communication.py
+++ src/communication.py
@@ -4,6 +4,8 @@ import pickle
 HEADER_SIZE = 4
 
 def getResponse(connection: socket.socket) -> dict | None:
+    '''Gets data from a socket'''
+    # Get the message size from the header
     messageSize = int.from_bytes(connection.recv(HEADER_SIZE), 'little')
     byte = connection.recv(messageSize)
 
@@ -15,10 +17,12 @@ def getResponse(connection: socket.socket) -> dict | None:
     return data
 
 def _sendHeader(connection: socket.socket, messageSize: int) -> None:
+    '''Sends a message header to defines the size of the message'''
     toSend = messageSize.to_bytes(HEADER_SIZE, 'little')
     connection.sendall(toSend)
 
 def sendResponse(connection: socket.socket, actionID: int = None, *args) -> None:
+    '''Sends a response to a socket'''
     data = {'actionID': actionID, 'data': [*args]}
     byte = pickle.dumps(data)
 
